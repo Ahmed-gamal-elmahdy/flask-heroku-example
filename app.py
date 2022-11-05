1d90dbf546176bef15d96710cd1479499da5b245
Ahmed-gamal-elmahdy <xda2014@hotmail.com>
2022-11-05 11:39:28 +0200

    finished flask project

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 import time
 
 # two decorators, same function
-@app.route('/')
-@app.route('/index.html')
+
 def index():
     path = "https://firebasestorage.googleapis.com/v0/b/fluttertest-24277.appspot.com/o/imgs%2Fanemia.jpg?alt=media&token=37cc6379-1263-474c-a0c8-ae2fb364a9f3"
     img=iio.imread(path)
@@ -16,7 +15,10 @@ def index():
     return str(end-start)
 
 
-
+@app.route('/')
+@app.route('/index.html')
+def home():
+    return "hi"
 
 if __name__ == '__main__':
     app.run(debug=True)
